b1d39f000de7bdab7b5bfe67cafddba635315e3a
Leonardo Duarte <hintl3rgames2015@gmail.com>
2022-10-13 12:31:03 -0300

    terminando o site

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,40 +1,9 @@
-// const controls = 
-// document.querySelectorAll(".control");
-// let currentItem = 0;
-// const items = 
-// document.querySelectorAll(".item");
-// const maxItems = items.length;
 
+ /* BOTAO RESPONSIVO MENU HAMBURGUER */
 
-
-// controls.forEach((control) => {
-//     control.addEventListener("click", () => {
-//         const isLeft = 
-//         control.classList.contains("arrow-left");
-
-
-//         if (isLeft) {
-//             currentItem -= 1;
-
-//         } else {
-//             currentItem += 1;
-//         }
-
-//         if (currentItem >= maxItems) {
-//          currentItem = 0;   
-//         }
-
-//         if (currentItem < 0) {
-//         currentItem = maxItems - 1;
-//         }
-
-//         items.forEach(item =>
-//              item.classList.remove('current-item'));
-
-//              items[currentItem].scrollIntoView({
-//                 inline: "center",
-//                 behavior: "smooth",
-//        });
-//        items[currentItem].classList.add("current-item");
-//     });
-// });
+ const btnMobile = document.getElementById('btnmobile')
+ function toggleMenu () {
+     const nav = document.getElementById('nav')
+     nav.classList.toggle('active')
+ }
+ btnMobile.addEventListener('click', toggleMenu)
